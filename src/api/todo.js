ad8066eb4eebe6363dfb9084a4d53914a312af95
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-08-12 08:00:56 +1000

    fix: /todo & /todo/{id}

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,8 +1,13 @@
 module.exports.handler = (event, ctx, done) => {
+  console.log(event);
   done(null, {
     statusCode: 200,
-    body: JSON.stringify({data: {
-      id: 1, name: 'clean up', status: 'open'
-    }})
-  })
-}
+    body: JSON.stringify({
+      data: {
+        id: 1,
+        name: "clean up",
+        status: "open"
+      }
+    })
+  });
+};
